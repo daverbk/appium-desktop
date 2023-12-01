decde26565dac763dca3fd553dfd571a2122a6c8
David Rabko <david.rabko@icloud.com>
2023-12-02 00:17:36 +0100

    Add base project structure

---
+++ AppiumDesktop/BaseTest.cs
@@ -0,0 +1,10 @@
+﻿namespace AppiumDesktop;
+
+public class BaseTest
+{
+    [OneTimeSetUp]
+    public void StartWindowsDriver() => WinAppDriverProvider.StartWinAppDriver();
+
+    [OneTimeTearDown]
+    public void StopCurrentDriver() => WinAppDriverProvider.StopWinAppDriver();
+}
